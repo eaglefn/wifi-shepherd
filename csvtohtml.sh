2c4a16b951079a9a6cb016255e801cf42ed2a1ff
eaglefn <64377595+eaglefn@users.noreply.github.com>
2020-04-28 14:21:59 +0200

    fix permissions

--- csvtohtml.sh
+++ csvtohtml.sh
@@ -9,7 +9,7 @@ echo "<table  style=\"width:100%\">"
 echo "<tr> <td><p align=\"left\"><font family=\"Verdana,Arial,Helvetica\" color=\"#0000FF\" size=\"+1\">"
 
 echo "Last Scan: `date +%Y-%m-%d" "%H:%M`"
-echo "</font></p> </td> <td><img src=\"wifi_shepherd.png\" alt=\"Wifi Shepherd\" width=\"225\" height=\"108\" align=\"right\"></td></tr></table>"
+echo "</font></p> </td> <td><img src=\"..\images\wifi_shepherd.png\" alt=\"Wifi Shepherd\" width=\"225\" height=\"108\" align=\"right\"></td></tr></table>"
 
 echo "<table id=\"shepherd\" class=\"display\" style=\"width:100%\">"
 echo "<thead>"
--- wifi-shepherd.sh
+++ wifi-shepherd.sh
@@ -12,17 +12,19 @@
 #------------------------------------------------------------------------------------------------------
 # Make your settings here!
 Wifi_Shepherd_Hostname="raspberrypi.lan"
-Wifi_Network="192.168.1.0/24"
+Wifi_Network="192.168.171.0/24"
 
 #MessageText="Alert! Wifi Shepherd found the following new devices in your network!"
 MessageText="Achtung! Wifi Shepherd hat folgende neuen Geräte in Ihrem Netzwerk gefunden!"
-EmailReceiver="yourname@email.com"
+EmailReceiver="frank@fnol.de"
+
 
 # Don't change unless you know what you are doing.
-Wifi_Shepherd_DIR="/home/pi/wifi-sheperd"
-Nmaptocsv_DIR=""$Wifi-Shepherd_DIR"/nmaptocsv"
+Wifi_Shepherd_DIR="/home/pi/wifi-shepherd/"
+Nmaptocsv_DIR=""$Wifi_Shepherd_DIR"nmaptocsv"
 Webserver_DIR="/var/www/html"
 
+Webserverindex_FILE="index.html"
 Nmapoutput_FILE="nmap.raw"
 Nmapcsv_FILE="nmap.csv"
 
@@ -38,20 +40,32 @@ MacDiff_FILE="mac_diff.txt"
 #Start nmap scan and save the output to nmap.raw
 #Exclude Wifi-Shepherd Hostname from scan
 cd  "$Wifi_Shepherd_DIR"
-nmap -sP  --exclude "$Wifi_Shepherd_Hostname" -oN  "$Wifi_Shepherd_DIR$Nmapoutput_FILE"  "$Wifi_Network" >/dev/null 2>&1
+sudo nmap -sP  --exclude "$Wifi_Shepherd_Hostname" -oN  "$Wifi_Shepherd_DIR$Nmapoutput_FILE"  "$Wifi_Network" >/dev/null 2>&1
+
+# Change ownership 
+sudo chown pi:pi "$Wifi_Shepherd_DIR$Nmapoutput_FILE"
 
 #Convert  nmap.raw output to csv file
 #See https://github.com/maaaaz/nmaptocsv for more information
-python "$Nmaptocsv_DIR"/nmaptocsv.py -i "$Nmapoutput_FILE"  -d ";" -n -f ip-fqdn-mac_address-mac_vendor > "$Nmapcsv_FILE"
+sudo python "$Nmaptocsv_DIR"/nmaptocsv.py -i "$Nmapoutput_FILE"  -d ";" -n -f ip-fqdn-mac_address-mac_vendor > "$Nmapcsv_FILE"
 
 # remove "" from  csv file
 cat  nmap.csv | sed 's/"//g' > nmap.tmp
 
-# copy html header to Webserver index.html
-cat html_head.txt > "$Webserver_DIR/index.html"
+# copy html header to Webserverindex
+cat html_head.txt > "$Webserverindex_FILE"
 
-#convert csv file to html and copy it to Webserver index.html
-./csvtohtml.sh nmap.tmp >>  "$Webserver_DIR/index.html"
+#convert csv file to html and copy to  Webserverindex
+./csvtohtml.sh nmap.tmp >>  "$Webserverindex_FILE"
+
+#Move Webserverindex to Webserver /var/www/html
+sudo mv  "$Webserverindex_FILE" "$Webserver_DIR"
+
+#Check if DataTables already exists on the webserver, if not copy all files
+DIRECTORY="/var/www/html/js/"
+if [ ! -d "$DIRECTORY" ]; then
+  sudo cp -R js/ images/ css/ "$Webserver_DIR"
+fi
 
 #Take csv file and cut Mac-Addresses, store it to MacDetected
 cat "$Nmapcsv_FILE"  | sed '1,1d' | cut -d ";" -f 3 > "$MacDetected_FILE"
@@ -73,7 +87,6 @@ cat  "$MacDiff_FILE" >> "$MacKnown_FILE"
 
 #Send email with alert if message.txt not empty
 
-if [ -s message.txt ];
-	then mail -s "$MessageText" "$EmailReceiver" < message.txt;
-fi
-
+#if [ -s message.txt ];
+#       then mail -s "$MessageText" "$EmailReceiver" < message.txt;
+#fi
