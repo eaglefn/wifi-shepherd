90aaddc9ae9eeb9a4b6c32970adfc254a6f6bccf
eaglefn <64377595+eaglefn@users.noreply.github.com>
2020-04-30 11:39:15 +0200

    Change DataTables layout

--- csvtohtml.sh
+++ csvtohtml.sh
@@ -5,13 +5,13 @@ CSV_FN=$1
 
 echo "<body>"
 
-echo "<table  style=\"width:100%\">"
-echo "<tr> <td><p align=\"left\"><font family=\"Verdana,Arial,Helvetica\" color=\"#0000FF\" size=\"+2\">"
+echo "<table align=\"center\" style=\"width:98%\">"
+echo "<tr> <td><p align=\"left\"><font family=\"Verdana,Arial,Helvetica\" color=\"#0000FF\" size=\"+1\">"
 
 echo "Last Scan: `date +%Y-%m-%d" "%H:%M`"
 echo "</font></p> </td> <td><img src=\"..\images\wifi_shepherd.gif\" alt=\"Wifi Shepherd\" width=\"225\" height=\"108\" align=\"right\"></td></tr></table>"
 
-echo "<table id=\"shepherd\" class=\"display\" style=\"width:100%\">"
+echo "<table id=\"shepherd\" class=\"table table-striped table-bordered\" style=\"width:98%\" align=\"center\">"
 echo "<thead>"
 head -n 1 $CSV_FN | \
     sed -e 's/^/<tr><th>/' -e 's/;/<\/th><th>/g' -e 's/$/<\/th><\/tr>/'
@@ -22,3 +22,4 @@ tail -n +2 $CSV_FN | \
 echo "</tbody>"
 echo "</table>"
 echo "</body>"
+
